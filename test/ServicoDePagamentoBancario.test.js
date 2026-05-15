11765e95a5bc36ca4c412e8c8889252c68d3c9f1
Wedney <wedney.santos@gmail.com>
2026-05-15 10:51:00 -0300

    Adicionando alterações nos valores dos pagamentos para validar a categoria

--- test/ServicoDePagamentoBancario.test.js
+++ test/ServicoDePagamentoBancario.test.js
@@ -11,7 +11,7 @@ describe('Testes para gestão de pagamentos bancários', function () {
 
     it('Validação do método realizar pagamento com valor menor que R$ 100,00 reais', function () {
         // Act (Agir)
-        const resultado = servicoDePagamentoBancario.pagar('0987-7656-3475', 'Minsait', 86.99, 'padrão');
+        const resultado = servicoDePagamentoBancario.pagar('0987-7656-9999', 'Minsait', 99.99, 'padrão');
         
         // Assert (Validar ou verificar)
         assert.equal(resultado, 'Pagamento realizado com sucesso.');
@@ -19,7 +19,7 @@ describe('Testes para gestão de pagamentos bancários', function () {
 
     it('Validação do método realizar pagamento com valor maior que R$ 100,00 reais', function () {
         // Act (Agir)
-        const resultado = servicoDePagamentoBancario.pagar('0988-7656-3475', 'Minsait', 100.01, 'cara');
+        const resultado = servicoDePagamentoBancario.pagar('0988-7656-1001', 'Minsait', 100.01, 'cara');
         
         // Assert (Validar ou verificar)
         assert.equal(resultado, 'Pagamento realizado com sucesso.');
@@ -33,15 +33,15 @@ describe('Testes para gestão de pagamentos bancários', function () {
 
     it('Validação do método consultar último pagamento quando houver algum pagamento feito', function () {
         // Arrange (Preparar)
-        servicoDePagamentoBancario.pagar('0987-7656-3475', 'Minsait', 86.99, 'padrão');
+        servicoDePagamentoBancario.pagar('0987-7656-9999', 'Minsait', 99.99, 'padrão');
         
         // Act (Agir)
         const ultimoPagamento = servicoDePagamentoBancario.consultarUltimoPagamento();
         
         // Assert (Agora validando ou verificando que o último pagamento deve retornar)
-        assert.equal(ultimoPagamento.codigoBarras, '0987-7656-3475');
+        assert.equal(ultimoPagamento.codigoBarras, '0987-7656-9999');
         assert.equal(ultimoPagamento.empresa, 'Minsait');
-        assert.equal(ultimoPagamento.valor, 86.99);
+        assert.equal(ultimoPagamento.valor, 99.99);
         assert.equal(ultimoPagamento.categoria, 'padrão');
     });
 });
